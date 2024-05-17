6cb4dee83186cf87236f6f0cdecb1227517dbca6
ateves <39039918+ateves@users.noreply.github.com>
2024-05-16 23:26:50 -0400

    updated methods and counters
    logic overall upheaved.

--- foodVotingApp.py
+++ foodVotingApp.py
@@ -1,25 +1,41 @@
-chineseFoodCount = int(0)
-mexicanFoodCount = int(0)
-americanFoodCount = int(0)
-
 def getName():
-    return input("enter your name: ")
+    return input("Enter your name: ")
 
 def getVote():
-    return input("Enter 1 for Chinese food, 2 for Mexican food, or 3 for American food: ") 
+    return input("Enter 1 for a Chinese food vote, 2 for a Mexican food vote, or 3 for an American food vote: ") 
 
 n1 = getName()
+vote = int(getVote())
 
-v1 = int(getVote())
+def chineseFoodCheck(voteNum):
+    chineseFoodCount = int(0)
+    if voteNum == 1:
+        chineseFoodCount += 1
+        print(n1 + " you have chosen Chinese food for your vote.")
+        return(chineseFoodCount)
+    else:
+        pass
 
-if v1 == 1:
-    chineseFoodCount += 1
-    print(chineseFoodCount)
-    print(n1 + " you have chosen Chinese food for your vote.")
-elif v1 == 2:
-    mexicanFoodCount += 1
-    print(n1 + " you have chosen Mexican food for your vote.")
-elif v1 == 3:
-    americanFoodCount += 1
-    print(n1 + " you have chosen American food for your vote")
+def mexicanFoodCheck(voteNum):
+    mexicanFoodCount = int(0)
+    if voteNum == 2:
+        mexicanFoodCount += 1
+        print(n1 + " you have chosen Mexican food for your vote.")
+        return(mexicanFoodCount)
+    else: 
+        pass
 
+def americanFoodCheck(voteNum):
+    americanFoodCount = int(0)
+    if voteNum == 3:
+        americanFoodCount += 1
+        print(n1 + " you have chosen American food for your vote")
+        return(americanFoodCount)
+    else: 
+        pass
+
+c = chineseFoodCheck(vote)
+m = mexicanFoodCheck(vote)
+a = americanFoodCheck(vote)
+
+print("Chinese food was voted for, " + str(c) + " times. Secondly, Mexican food was voted for, " + str(m) + " times. And lastly, American food was voted for, " + str(a) + " times.")
